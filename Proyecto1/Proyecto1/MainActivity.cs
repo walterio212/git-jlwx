b3dbd21a21d93829289d91564ff1f7bbf42199b0
Walter <walter.dav.093@gmail.com>
2017-02-05 16:57:12 -0300

    primer check in

---
+++ Proyecto1/Proyecto1/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Proyecto1
+{
+    [Activity(Label = "Proyecto1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
